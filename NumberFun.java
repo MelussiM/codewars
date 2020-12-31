50435a9a67c061b54f3943b8234fd9e896520252
MelussiM <76811639+MelussiM@users.noreply.github.com>
2020-12-31 15:10:36 +0200

    Update NumberFun.java

--- NumberFun.java
+++ NumberFun.java
@@ -3,24 +3,38 @@ public class NumberFun
   public static long findNextSquare(long sq) 
   {
       long one = 2;
-    long onee = 1;
-    double pS = Math.sqrt(sq);
-    //if((long)Math.floor((long)Math.sqrt(sq)) % one != 0)
-    if((pS - (long)Math.floor(pS)) != 0)
-    {
-        
-         return -1; 
-    }
-    else
-    {
-      //if(Math.sqrt(sq+1) % 1 != 0)
-      //{
-       long nextSq = (long)Math.floor((long)Math.sqrt(sq))+onee;
+      long onee = 1;
+      double pS = Math.sqrt(sq);
+      if((pS - (long)Math.floor(pS)) != 0)
+      {
+        return -1; 
+      }
+      else
+      {
+        long nextSq = (long)Math.floor((long)Math.sqrt(sq))+onee;
         return nextSq*nextSq;
-       
-      //}
-      //sq++;
-    } 
-    //return sq;
+      }
   }
+}
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+
+public class FindNextSquareTest {
+    @Test
+    public void test1() {
+        assertEquals(144, NumberFun.findNextSquare(121));
+    }
+    
+    @Test
+    public void test2() {
+        assertEquals(676, NumberFun.findNextSquare(625));
+    }
+    
+    @Test
+    public void test3() {
+        assertEquals(-1, NumberFun.findNextSquare(114));
+    }  
 }
