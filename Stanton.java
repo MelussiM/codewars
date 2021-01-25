c48989a832fe2ea60ea45aa5e728bae827d21704
MelussiM <76811639+MelussiM@users.noreply.github.com>
2021-01-25 16:40:20 +0200

    Create Stanton.java
    //The Stanton measure of an array is computed as follows: count the number of 1s in the array. Let this count be n. The Stanton measure is the number of times that n appears in the array.
    
    //Write a function which takes an integer array and returns its Stanton measure.
    
    //Example
    //The Stanton measure of [1, 4, 3, 2, 1, 2, 3, 2] is 3, because 1 occurs 2 times in the array and 2 occurs 3 times

---
+++ Stanton.java
@@ -0,0 +1,35 @@
+//The Stanton measure of an array is computed as follows: count the number of 1s in the array. Let this count be n. The Stanton measure is the number of times that n appears in the array.
+
+//Write a function which takes an integer array and returns its Stanton measure.
+
+//Example
+//The Stanton measure of [1, 4, 3, 2, 1, 2, 3, 2] is 3, because 1 occurs 2 times in the array and 2 occurs 3 times
+
+public class Stanton {
+
+  public static int stantonMeasure(int[] arr) {
+    // Your code here
+    int count = 0;
+    int s = 0;
+    for(int i =  0; i < arr.length; i++)
+    {
+        if(arr[i] == 1)
+        {
+          count += 1;
+           
+        }
+       
+    }
+    
+    for(int i =  0; i < arr.length; i++)
+    {
+     
+        if(count == arr[i])
+        {
+            s += 1;
+        }
+     }
+    return s;
+  }
+  
+}
